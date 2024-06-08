330c1b7215e0334eb710bcd0c55b9a061d940bd3
Sergei Liebich <sergei.liebich@deutschebahn.com>
2024-06-08 14:19:29 +0200

    Integration and configuration of nuxt/i18n

--- nuxt_tailwindcss_content_i18n/nuxt.config.ts
+++ nuxt_tailwindcss_content_i18n/nuxt.config.ts
@@ -1,9 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss', 
-    '@nuxtjs/i18n', 
-    '@nuxt/content'
-  ]
-})
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxt/content"],
+  i18n: {
+    locales: [
+      {
+        code: "en",
+        iso: "en-US",
+        name: "English",
+        file: "en-US.json",
+      },
+      {
+        code: "de",
+        iso: "de-DE",
+        name: "Deutsch",
+        file: "de-DE.json",
+      },
+    ],
+    defaultLocale: "en",
+    langDir: "locales/",
+    strategy: "prefix_except_default",
+  },
+});
---
+++ nuxt_tailwindcss_content_i18n/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1 class="mt-4 text-4xl font-bold text-center">
+      {{ $t("headline") }}
+    </h1>
+    <p class="text-center">{{ $t("subheadline") }}</p>
+    <form class="text-center">
+      <select v-model="locale">
+        <option value="en">English</option>
+        <option value="de">Deutsch</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { locale } = useI18n();
+</script>
